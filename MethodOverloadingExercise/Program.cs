4dfa45b185aae1c75035b228aae6f935882c99ae
bryan mcwatters <bmcwatters@comcast.net>
2021-12-05 19:07:25 -0500

    init commit

---
+++ MethodOverloadingExercise/Program.cs
@@ -0,0 +1,45 @@
+﻿
+
+Console.WriteLine(Add(5, 0, true));
+
+/*
+    static int Add(int num1, int num2) //Method adding two numbers
+    {
+        return num1 + num2;
+    }
+
+
+     static decimal Add(decimal num1, decimal num2) //Method Overload adding two decimal num
+    {
+        return num1 + num2;
+    }
+*/
+
+    static string Add(int num1, int num2, bool isTrue)//Method Overload Add returning string
+    {
+        var sum = num1 + num2;
+
+        if (isTrue == true && sum > 1)
+        {
+
+            return $"{sum} dollars";
+        }
+        else if (isTrue == true && sum == 1)
+        {
+            return $"{sum} dollar";
+        }
+        else if (isTrue == true && sum < 1)
+        {
+            return $"{sum} dollars";
+        }
+        else
+        {
+            return sum.ToString();
+        }
+
+    }
+
+
+
+
+
